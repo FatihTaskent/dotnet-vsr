9e21037c92076ae8bb92672a9d40238afe27a600
Fatih75 <fatih-taskent@hotmail.com>
2017-02-01 17:17:20 +0100

    fix namespace error

--- Migrations/20170201152008_setup.Designer.cs
+++ Migrations/20170201152008_setup.Designer.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using DataAcces;
+using DataAccess;
 
 namespace dotnetvsr.Migrations
 {
