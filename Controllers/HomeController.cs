ca1a8f234b986c7386a87261f834b91391fd07a5
ismailgurler1 <igurler@avans.nl>
2017-02-01 20:13:47 +0000

    Messages implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnet_core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_vsr.Controllers
 {
@@ -10,8 +12,19 @@ namespace dotnet_vsr.Controllers
     {
         public IActionResult Index()
         {
+            var c = new DataAccess.DatabaseContext();
+            Account account = null;
+            try
+            {
+                account = c.Accounts.Include(x => x.Messages).First();
+            }
+            catch (InvalidOperationException ioex) { }
+
+            ViewData["Account"] = account;  
+            ViewData["Message"] = "Welcome";
             return View();
         }
+
 
         public IActionResult About()
         {
@@ -29,6 +42,21 @@ namespace dotnet_vsr.Controllers
 
         public IActionResult Error()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Message model)
+        {
+            var c = new DataAccess.DatabaseContext();
+            if(model != null)
+            {
+                var a = c.Accounts.Include(x => x.Messages).First();
+                model.Account = a;
+                a.Messages.Add(model);
+                ViewData["Account"] = a;
+                c.SaveChanges();
+            }
             return View();
         }
     }
